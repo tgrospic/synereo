0ea664220e784b2f732551189092caadd21821c6
Henry Till <henrytill@gmail.com>
2016-09-30 15:30:12 -0400

    checkpoint multi-node config files and scripts

--- gloseval/scripts/rabbitTestN1.scala
+++ gloseval/scripts/rabbitTestN1.scala
@@ -9,10 +9,10 @@ import com.biosimilarity.lift.lib._
 import com.biosimilarity.lift.lib.usage.AMQPTPSample._ 
 import _root_.com.rabbitmq.client.{Channel=>RabbitChan, _} 
 
-val sIP="192.168.99.100"   
-val tIP="192.168.99.100"
-val sourceHost1 = new URI(s"amqp://guest:guest@$sIP:5672/synereo" )  
-val targetHost1 = new URI(s"amqp://guest:guest@$tIP:5672/synereo" ) 
+val sIP="127.0.0.1"
+val tIP="127.0.0.1"
+val sourceHost1 = new URI(s"amqp://guest:guest@$sIP:6672/synereo" )
+val targetHost1 = new URI(s"amqp://guest:guest@$tIP:5672/synereo" )
 
 //For first node                                                                 
 setupAndRunTest( true, sourceHost1, targetHost1, "synereo1", true, 10)
--- gloseval/scripts/rabbitTestN2.scala
+++ gloseval/scripts/rabbitTestN2.scala
@@ -9,10 +9,10 @@ import com.biosimilarity.lift.lib._
 import com.biosimilarity.lift.lib.usage.AMQPTPSample._ 
 import _root_.com.rabbitmq.client.{Channel=>RabbitChan, _} 
 
-val sIP="192.168.99.100"   
-val tIP="192.168.99.100"
-val sourceHost1 = new URI(s"amqp://guest:guest@$sIP:5672/synereo" )  
-val targetHost1 = new URI(s"amqp://guest:guest@$tIP:5672/synereo" ) 
+val sIP="127.0.0.1"
+val tIP="127.0.0.1"
+val sourceHost1 = new URI(s"amqp://guest:guest@$sIP:5672/synereo" )
+val targetHost1 = new URI(s"amqp://guest:guest@$tIP:6672/synereo" )
 
 //For second node 
 setupAndRunTest( false, sourceHost1, targetHost1, "synereo1", true, 10)
